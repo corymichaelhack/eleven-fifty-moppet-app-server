8ffb8ede08221e3b7a5321fc3b2561c6d568d476
Cory Hack <cory.hack@gmail.com>
2019-11-04 21:31:16 -0500

    Initial commit with databse setup and express app setup

---
+++ app.js
@@ -0,0 +1,16 @@
+//create variable to require express
+var express = require('express');
+var app = express();
+
+var sequelize = require('./db');
+
+// call sync() method to define all models in DB
+sequelize.sync(); // {force:true} to drope tables in database
+
+
+
+
+
+app.listen(3000, function(){
+    console.log('App is listening on port 3000')
+});
---
+++ db.js
@@ -0,0 +1,31 @@
+// 1 - import the Sequelize package
+const Sequelize = require('sequelize');
+
+// 2 - create new instance of Sequelize for use in the module with the variable sequelize
+// 3 - use the constructor to create a new Sequelize
+// 4 - pass in db table to connect to 
+//5 - pass in the username for the bd
+// 6 - pass in the password for the db
+const sequelize = new Sequelize('moppetDB', 'postgres', 'password', {
+    // 7 - points to the local port 5432
+    host: 'localhost', 
+    // 8 - identifty the QL dialect, for example SQLLite, postgresql, others
+    dialect: 'postgress'
+});
+
+// 9 - Use sequelize vairbale to access methods
+// 10 - one such method is the autheniticate, call authenticate
+// 11 - authenticate returns a promise with a returned promise we use .then()
+sequelize.authenticate().then(
+    // 12 - fire a function to show connected to the database
+    function(){
+        console.log('Conntected to MoppetDB')
+    },
+    //  13 - followed by a functions if any errors
+    function(err){
+        console.log(err);
+    }
+);
+
+// 14 - export the module
+module.exports = sequelize;
