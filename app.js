35ec53a6b5f40771c43262676cbbb30109f6dc2b
Cory Hack <cory.hack@gmail.com>
2019-11-16 09:15:49 -0500

    Fix Add child in heroku 002

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/moppet/child/addnewchild', upload.single('image'), async (req, res, 
             meds: newmeds,
             allergy: newallergy,
             imageUrl: newimage
-    })
+    }).catch(err => res.send(500, err.message))
     } else {
         //NO PHOTO UPLOADED
         let newfirstName = req.body.child.firstName;
@@ -75,7 +75,7 @@ app.post('/moppet/child/addnewchild', upload.single('image'), async (req, res, 
                 meds: newmeds,
                 allergy: newallergy,
                 // imageUrl: newimage
-        })
+        }).catch(err => res.send(500, err.message))
     }
    
 });
